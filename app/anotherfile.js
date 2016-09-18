8e59cafbcbc30af2c241a67793527992d72de62e
marlass <lasakmarcin90@gmail.com>
2016-09-18 16:27:58 +0200

    Centrailized tests file

---
+++ app/anotherfile.js
@@ -0,0 +1,8 @@
+module.exports = function testFunction(a) {
+    if (a) {
+        return 'blob';
+    } else {
+        return 'foo';
+    }    
+    return 'yo';
+}
--- tests/anotherfile.js
+++ tests/anotherfile.js
@@ -1,17 +1,17 @@
 const test = require('tape');
 
-const printer = require('../app/index.js');
+const testFunction = require('../app/anotherfile.js');
 
 module.exports = function(){
-    
+
 test('printing function', function(t) {
-    t.equal(printer(), 'Hello world!');
+    t.equal(testFunction(!0), 'blob');
     t.end();
 });
 
 test('failing test', function(t) {
-    t.equal('fail','fail');
+    t.equal(testFunction(0), 'foo');
     t.end();
-});
+})
 
 }
