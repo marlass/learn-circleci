f1c87c9d5b7a364734eb4122ba8dfee30f065da5
marlass <lasakmarcin90@gmail.com>
2016-09-18 16:32:34 +0200

    Change tests names

--- tests/anotherfile.js
+++ tests/anotherfile.js
@@ -4,12 +4,12 @@ const testFunction = require('../app/anotherfile.js');
 
 module.exports = function(){
 
-test('printing function', function(t) {
+test('testFunction', function(t) {
     t.equal(testFunction(!0), 'blob');
     t.end();
 });
 
-test('failing test', function(t) {
+test('testFunction false', function(t) {
     t.equal(testFunction(0), 'foo');
     t.end();
 })
